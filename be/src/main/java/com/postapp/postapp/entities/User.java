8d120cc026d4e2e473a5244a9636b96e0a54ee19
M. Onur Aysel <93475143+LyotardPostmodernizm@users.noreply.github.com>
2025-04-30 17:21:32 +0300

    Entities and relations between them are created. React initialized with Vite.

---
+++ be/src/main/java/com/postapp/postapp/entities/User.java
@@ -0,0 +1,38 @@
+package com.postapp.postapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int avatar;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "user")
+    private List<Like>likes;
+
+
+
+
+}
